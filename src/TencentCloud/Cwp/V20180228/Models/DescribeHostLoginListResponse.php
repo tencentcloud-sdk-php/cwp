17da288c8438c3b688343f2d2b2f4aa42b3a662b
tencentcloudapi <tencentcloudapi@tencent.com>
2025-04-14 04:35:46 +0800

    release 3.0.1359

--- src/TencentCloud/Cwp/V20180228/Models/DescribeHostLoginListResponse.php
+++ src/TencentCloud/Cwp/V20180228/Models/DescribeHostLoginListResponse.php
@@ -22,8 +22,8 @@ use TencentCloud\Common\AbstractModel;
  *
  * @method integer getTotalCount() 获取总数
  * @method void setTotalCount(integer $TotalCount) 设置总数
- * @method array getHostLoginList() 获取登录审计列表
- * @method void setHostLoginList(array $HostLoginList) 设置登录审计列表
+ * @method array getHostLoginList() 获取异常登录列表
+ * @method void setHostLoginList(array $HostLoginList) 设置异常登录列表
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
@@ -35,7 +35,7 @@ class DescribeHostLoginListResponse extends AbstractModel
     public $TotalCount;
 
     /**
-     * @var array 登录审计列表
+     * @var array 异常登录列表
      */
     public $HostLoginList;
 
@@ -46,7 +46,7 @@ class DescribeHostLoginListResponse extends AbstractModel
 
     /**
      * @param integer $TotalCount 总数
-     * @param array $HostLoginList 登录审计列表
+     * @param array $HostLoginList 异常登录列表
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
